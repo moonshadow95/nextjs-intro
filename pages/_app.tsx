26e9229e3f0aaa61dce00dd8f0f8d03a7ae8859c
Moonshadow <last_enemy@naver.com>
2022-09-20 14:34:41 +0900

    Custom App

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from 'next/app';
+import NavBar from '../components/NavBar';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
+          a {
+            color: green;
+          }
+        `}
+      </style>
+    </>
+  );
+}
