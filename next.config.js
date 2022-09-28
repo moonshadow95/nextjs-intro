4010b0b7be6c8071cf8281ea18bf9cf573e34cab
ictus <last_enemy@naver.com>
2022-09-28 16:04:44 +0900

    Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
-/** @type {import('next').NextConfig} */
+/** @type {{redirects(): Promise<[{permanent: boolean, destination: string, source: string}]>, reactStrictMode: boolean, swcMinify: boolean}} */
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects(){
+    return[
+      {
+        source:'/old/:path*',
+        destination:'/new/:path*',
+        // old 로 들어오면 new 로 보내고 :path 의 모든 내용을 캐치하여 그대로 보내준다.
+        // 실제로 url 이 변경된다.
+        permanent:false
+      }
+    ]
+  },
+  async rewrites(){
+    return[{
+      source:'/api/movies',
+      destination:`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      // m
+    }]
+  }
 }
 
 module.exports = nextConfig
