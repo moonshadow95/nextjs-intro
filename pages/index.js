122b57719f69d1b082f8addff8c42a2cb9c7e9c8
ictus <last_enemy@naver.com>
2022-10-18 11:21:57 +0900

    Catch all

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import Seo from '../components/Seo';
+import {useRouter} from "next/router";
 
 export default function Home({results}) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`)
+  }
   return (
     <div className="container">
-      <Seo title="Home" />
+      <Seo title="Home"/>
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.title}</h4>
+        <div onClick={() => onClick(movie.id, movie.title)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
+          <h4>
+            {movie.title}
+          </h4>
         </div>
+
       ))}
       <style jsx>{`
         .container {
@@ -17,15 +25,22 @@ export default function Home({results}) {
           padding: 20px;
           gap: 20px;
         }
+
+        .movie {
+          cursor: pointer;
+        }
+
         .movie img {
           max-width: 100%;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
+
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
         }
+
         .movie h4 {
           font-size: 18px;
           text-align: center;
@@ -35,14 +50,14 @@ export default function Home({results}) {
   );
 }
 
-export async function getServerSideProps(){
-  const { results } = await (
+export async function getServerSideProps() {
+  const {results} = await (
     await fetch(
-      'http://localhost:3001/api/movies'
+      'http://localhost:3000/api/movies'
     )
   ).json();
   return {
-    props:{
+    props: {
       results
     }
   }
