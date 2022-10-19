0d40e9e1482910c9c21f493903a1eb6591325c04
ictus <last_enemy@naver.com>
2022-10-19 10:32:32 +0900

    404 not found

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,10 +1,7 @@
-import {useRouter} from "next/router";
 import Seo from "../../components/Seo";
 
-export default function Detail() {
-  const router = useRouter()
-  const [title, id] = router.query.params
-  console.log(router)
+export default function Detail({params}) {
+  const [title, id] = params
   return <div>
     <Seo title={title}/>
     <h4>{title}</h4>
